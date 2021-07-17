05c6f8128cbea64c37d4d1a878da6625e93765b5
vsae8538 <vsae8538@gmail.com>
2021-07-17 19:32:35 +0800

    gradle build init

--- src/main/java/com/github/vsae/fm/FmBackOfficeApplication.java
+++ src/main/java/com/github/vsae/fm/FmBackOfficeApplication.java
@@ -1,4 +1,13 @@
 package com.github.vsae.fm;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FmBackOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmBackOfficeApplication.class, args);
+    }
+
 }
